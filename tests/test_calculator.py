d0a56008475770c50933e3518546125df4b1a6a2
Carlo David <carlo12david@gmail.com>
2023-06-24 23:31:59 +0000

    add tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,14 @@
+from calculator import add, sub, mul, div
+
+
+def test_add():
+    assert add(2, 2) == 4
+
+def test_sub():
+    assert sub(7, 3) == 4
+
+def test_mul():
+    assert mul(2, 2) == 4
+
+def test_div():
+    assert div(8, 2) == 4
